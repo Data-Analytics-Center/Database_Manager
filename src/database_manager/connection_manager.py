1bcc283c67ee32d027b0720a57159af1b263d64d
Kevin Henderson <24301654+khenderson20@users.noreply.github.com>
2023-08-15 17:12:18 -0500

    I did the best I could, will return to fight again another day

--- src/database_manager/connection_manager.py
+++ src/database_manager/connection_manager.py
@@ -1,14 +1,33 @@
 """Contains database connection and execution logic."""
 
 from sqlalchemy import create_engine, text
+from sqlalchemy.engine.base import Connection
+import os
 
+def create_connection() -> Connection:
+    """Create a connection object to a database.
 
-def create_connection(driver: str, server: str, database: str):
-    """Create a connection object to a database."""
-    conn_string = f"Driver={driver};Server={server};Database={database};Trusted_Connection=yes;Encrypt=no;"
-    engine = create_engine()
-    # Brandon TODO: need to make sure this is the correct way
-    return engine.begin(conn_string)
+    The function relies on enviroment variables for the connection parameters
+    Returns:
+        A SQLAlchemy engine connection object.
+    """
+    environment_type = os.environ.get('ENV_TYPE')
+    database = os.environ.get('DATABASE')
+    driver = os.environ.get(f'{environment_type}_DRIVER', '')
+    server = os.environ.get(f'{environment_type}_SERVER', '')
+    
+    if database is None or database == '':
+        raise ValueError('Database name cannot be empty.')
+    if driver is None or driver == '':
+        raise ValueError('Driver cannot be empty.')
+    if server is None or server == '':
+        raise ValueError('Server cannot be empty.')
+    if environment_type is None or environment_type == '':
+        raise ValueError('Environment type cannot be empty.')
+    
+    connection_string = f"mssql://@{server}/{database}?driver={driver}&Encrypt=no"
+    engine = create_engine(connection_string)
+    return engine.begin()
 
 
 def execute_query(conn, sql, values=None):
--- src/tests/test_create_connection.py
+++ src/tests/test_create_connection.py
@@ -1,10 +1,33 @@
-# test_create_connection()
+"""
+This module contains the unit tests for the database_manager module.
 
-from unittest import TestCase
+In this case, the create_engine() function is an external dependency, so you can use unittest.mock to replace it with a mock object during testing.
 
-class TryTesting(TestCase):
-    def test_create_connection(self):
-        self.assertTrue(True)
+- Kevin Henderson 2023 8/15/2023
+TODO: Figure out how to mock the return value of create_connection() to return a fake Connection object
+- Connection Object Docs
+https://docs.sqlalchemy.org/en/20/core/engines.html
+
+- Mocking SQLAlchemy
+https://pypi.org/project/mock-alchemy/
+
+"""
+from unittest.mock import MagicMock, patch, mock
+from ..database_manager import create_connection
+@mock.patch('database_manager.connection_manager.create_connection', return_value='test_connection')
+def test_create_connection(self, mock_create_connection):
+    # Define fake environment variables
+    fake_env = {
+        'ENV_TYPE': 'TEST',
+        'DATABASE': 'test_db',
+        'TEST_DRIVER': 'test_driver',
+        'TEST_SERVER': 'test_server'
+    }
     
-    def test_always_fails(self):
-        self.assertTrue(False)
+    # Define the expected connection string
+    expected_connection_string = "mssql://@test_server/test_db?driver=test_driver&Encrypt=no"
+    
+    # Create the MagicMock object for the engine and set up the begin method to return the fake connection as a context manager
+    mock_engine = MagicMock(spec=Connection)
+    mock_engine.begin.return_value.__enter__.return_value = 'test_connection'
+
