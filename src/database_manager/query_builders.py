4c6cfe7cc0baa190839ed762ce560c4ddaaa7269
Poonacha Cheppudira <poonachausa@gmail.com>
2023-08-14 17:33:28 -0700

    Moved simple insert to query builders folder

--- src/database_manager/query_builders.py
+++ src/database_manager/query_builders.py
@@ -1,1 +1,28 @@
+from .connection_manager import create_connection
+
 """Defines all the query builders for all database operations."""
+
+
+def simple_insert(database: str, table: str, columns: list, *args) -> None:
+    """Insert a single row into a table"""
+    if not database:
+        raise ValueError("The database name is not provided!")
+    if not table:
+        raise ValueError("The table name is not provided!")
+    if not columns:
+        raise ValueError("At least one column is required!")
+    if len(columns) != len(args):
+        raise ValueError(
+            f"Number of columns does not match the number of args provided!"
+        )
+
+    conn = create_connection(database)
+    if not conn:
+        raise Exception(f"The connection was not formed with this database: {database}")
+
+    column_string = ", ".join(columns)
+    placeholders = ", ".join(["?" for _ in args])
+    query = f"""INSERT INTO {table} ({column_string}) VALUES ({placeholders});"""
+
+    conn.execute(query)
+    conn.commit()
