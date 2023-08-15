2f4191f6ca082181dd74fddf4ec05f799baa6520
Brandon Muller <lift.off.to.andromeda@gmail.com>
2023-08-15 15:58:48 -0500

    added env variables and template

--- src/database_manager/connection_manager.py
+++ src/database_manager/connection_manager.py
@@ -1,27 +1,57 @@
 """Contains database connection and execution logic."""
 
+import os
+
 from sqlalchemy import create_engine, text
 from sqlalchemy.engine.base import Connection
 
 
-def create_connection(driver: str, server: str, database: str) -> Connection:
+def create_connection() -> Connection:
     """Create a connection object to a database.
 
-    Arguments:
-        driver: The driver that connects to the database.
-        server: The name of the database server.
-        database: The database to connect to.
+    This function creates a SQLAlchemy engine connection object to a database. 
+    It relies on environment variables for the connection parameters, 
+    including the driver, server, database, and environment type.
+
+    Environment Variables:
+        - `ENV_TYPE`: The environment type (e.g., 'DEV', 'PROD', 'TEST').
+        - `DATABASE`: The name of the database to connect to.
+        - `{ENV_TYPE}_DRIVER`: The driver for the connection (e.g., 'ODBC Driver 17 for SQL Server').
+        - `{ENV_TYPE}_SERVER`: The server name or address for the connection.
 
     Returns:
-        A SQLAlchemy engine connection object.
+        Connection: A SQLAlchemy engine connection object.
+
+    Raises:
+        ValueError: If any of the required environment variables are empty or not set.
+
+    Example:
+        To use this function, set the environment variables and then call `create_connection()`:
+        ```python
+        import os
+        os.environ['ENV_TYPE'] = 'development'
+        os.environ['DATABASE'] = 'my_database'
+        os.environ['development_DRIVER'] = 'ODBC Driver 17 for SQL Server'
+        os.environ['development_SERVER'] = 'my_server_address'
+        
+        connection = create_connection()
+        ```
     """
-    params = {'Driver': driver, 'Server': server, 'Database': database}
+    environment_type = os.environ.get('ENV_TYPE')
+    database = os.environ.get('DATABASE')
+    driver = os.environ.get(f'{environment_type}_DRIVER')
+    server = os.environ.get(f'{environment_type}_SERVER')
     
-    for name, value in params.items():
-        if value is None or value == "":
-            raise ValueError(f"{name} cannot be None or empty")
+    if database is None or database == '':
+        raise ValueError('Database name cannot be empty.')
+    if driver is None or driver == '':
+        raise ValueError('Driver cannot be empty.')
+    if server is None or server == '':
+        raise ValueError('Server cannot be empty.')
+    if environment_type is None or environment_type == '':
+        raise ValueError('Environment type cannot be empty.')
     
-    connection_string = f"Driver={driver};Server={server};Database={database};Trusted_Connection=yes;Encrypt=no;"
+    connection_string = f"mssql://@{server}/{database}?driver={driver}&Encrypt=no"
     engine = create_engine(connection_string)
     return engine.begin()
 
@@ -34,6 +64,9 @@ def execute_query(connection: Connection, query: str, values:tuple[any, ...]=Non
         connection: The connnection object to the database.
         query: The query to execute.
         values: The values to pass to the query if any.
+
+    Raises:
+        Exception: If anything goes wrong with the database transaction.
     """
     try:
         if values is not None:
