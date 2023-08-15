419f40c6ae70a0bd544936de496bdca90b1b7d63
Poonacha Cheppudira <poonacha.cheppudira@utsa.edu>
2023-08-14 17:03:43 -0700

    Added simple_insert function for connection_manager.py

--- src/database_manager/connection_manager.py
+++ src/database_manager/connection_manager.py
@@ -16,3 +16,28 @@ def execute_query(conn, sql, values=None):
     # Brandon TODO: need to update properly
     conn.execute(text(sql))
     conn.close()
+
+
+def simple_insert(database: str, table: str, columns: list, *args) -> None:
+    """Insert a single row into a table"""
+    if not database:
+        raise ValueError("The database name is not provided!")
+    if not table:
+        raise ValueError("The table name is not provided!")
+    if not columns:
+        raise ValueError("At least one column is required!")
+    if len(columns) != len(args):
+        raise ValueError(
+            f"Number of columns does not match the number of args provided!"
+        )
+
+    conn = create_connection(database)
+    if not conn:
+        raise Exception(f"The connection was not formed with this database: {database}")
+
+    column_string = ", ".join(columns)
+    placeholders = ", ".join(["?" for _ in args])
+    query = f"""INSERT INTO {table} ({column_string}) VALUES ({placeholders});"""
+
+    conn.execute(query)
+    conn.commit()
