907729a367ebebed7e986bc6d7a6e303b39d5df6
Brandon Muller <lift.off.to.andromeda@gmail.com>
2023-08-15 15:12:39 -0500

    added code to make into package

--- src/database_manager/connection_manager.py
+++ src/database_manager/connection_manager.py
@@ -1,18 +1,47 @@
 """Contains database connection and execution logic."""
 
 from sqlalchemy import create_engine, text
+from sqlalchemy.engine.base import Connection
 
 
-def create_connection(driver: str, server: str, database: str):
-    """Create a connection object to a database."""
-    conn_string = f"Driver={driver};Server={server};Database={database};Trusted_Connection=yes;Encrypt=no;"
-    engine = create_engine()
-    # Brandon TODO: need to make sure this is the correct way
-    return engine.begin(conn_string)
+def create_connection(driver: str, server: str, database: str) -> Connection:
+    """Create a connection object to a database.
+
+    Arguments:
+        driver: The driver that connects to the database.
+        server: The name of the database server.
+        database: The database to connect to.
+
+    Returns:
+        A SQLAlchemy engine connection object.
+    """
+    params = {'Driver': driver, 'Server': server, 'Database': database}
+    
+    for name, value in params.items():
+        if value is None or value == "":
+            raise ValueError(f"{name} cannot be None or empty")
+    
+    connection_string = f"Driver={driver};Server={server};Database={database};Trusted_Connection=yes;Encrypt=no;"
+    engine = create_engine(connection_string)
+    return engine.begin()
 
 
-def execute_query(conn, sql, values=None):
-    """Execute an SQL query."""
-    # Brandon TODO: need to update properly
-    conn.execute(text(sql))
-    conn.close()
+
+def execute_query(connection: Connection, query: str, values:tuple[any, ...]=None) -> None:
+    """Execute an SQL query.
+
+    Arguments:
+        connection: The connnection object to the database.
+        query: The query to execute.
+        values: The values to pass to the query if any.
+    """
+    try:
+        if values is not None:
+            connection.execute(text(query), **values)
+        else:
+            connection.execute(text(query))
+    except Exception as e:
+        print(e)
+        raise e
+    finally:
+        connection.close()
---
+++ src/database_manager/tests/test_build.py
@@ -0,0 +1,5 @@
+"""Used to test that the package builds and installs correctly, run after building th eproject and installing the package."""  # noqa: E501
+
+def test_build():
+    """Build Test method."""
+    return "Hello World"
