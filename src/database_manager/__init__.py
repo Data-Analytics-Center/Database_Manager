b4815ffacca97e17576654e34f7bea2f2e602d3b
Brandon Muller <brandon.muller@utsa.edu>
2023-08-16 15:51:46 -0700

    started pandas integration

--- src/database_manager/__init__.py
+++ src/database_manager/__init__.py
@@ -1,6 +1,6 @@
 """Defines the package's API aka what functions the user may access."""
 
-from .connection_manager import create_connection, execute_query
-from .query_builders import select
+from .connection_manager import create_engine, execute_query
+from .queries import select
 
-__all__ = ["create_connection", "execute_query"]
+__all__ = ["create_engine", "execute_query"]
--- src/database_manager/tests/test_build.py
+++ src/database_manager/tests/test_build.py
@@ -1,5 +1,6 @@
 """Used to test that the package builds and installs correctly, run after building th eproject and installing the package."""  # noqa: E501
 
+
 def test_build():
     """Build Test method."""
     return "Hello World"
