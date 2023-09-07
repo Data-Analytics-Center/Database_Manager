66f159e04315039ba5267afc059c18f8ed0a24e7
Sarthak <srnegi141@gmail.com>
2023-09-07 09:23:08 -0500

    removed fake test

--- test.py
+++
@@ -1,9 +0,0 @@
-from src.database_manager.query_builders import build_insert_query
-
-sql = build_insert_query(
-    table="dbo.MyTable",
-    columns=["ID", "Name"],
-    data_rows=[(None, "Sarthak"), (2, "John")],
-)
-
-print(sql)
