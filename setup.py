3294486826d7d911a7907916423591582162f013
ebachUTSA <eric.bachura@utsa.edu>
2025-01-29 16:06:18 -0600

    Updated version num

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as f:
 
 setup(
     name="database_manager",
-    version="0.0.2",
+    version="0.0.3",
     description="Database Manager package",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
